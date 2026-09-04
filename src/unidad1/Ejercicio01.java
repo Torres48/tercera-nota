8ffc0de4f2b156c67c0d50c1591c22b3ebcb3b3a
Torres48 <yobelsacastilla@gmail.com>
2026-09-04 18:49:56 -0500

    Estructura para la entrega de tercera nota

---
+++ src/unidad1/Ejercicio01.java
@@ -0,0 +1,10 @@
+package unidad1;
+
+/**
+ * Ejercicio01
+ */
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        System.out.println("primer ejercicio unidad 1");
+    }
+}
